2273735a65bfb2d1599d4c537ab969079636bc2f
6sn <1437641239@qq.com>
2026-06-25 10:27:38 +0800

    feat: export all 14 charts from top-level API

--- src/acadp/__init__.py
+++ src/acadp/__init__.py
@@ -5,6 +5,7 @@ __version__ = "0.1.0"
 from acadp.charts import (
     lineplot, barplot, scatter, heatmap, boxplot,
     violinplot, histogram, radar, area, stacked_bar,
+    pareto, contour, waterfall, dumbbell,
 )
 from acadp._style import set_style, get_style, set_dpi, set_font, set_context
 from acadp._suggest import suggest, auto_plot, AutoPlotResult
@@ -13,6 +14,7 @@ from acadp._reviewer import review, review_dir, ReviewResult, BatchReport
 __all__ = [
     "lineplot", "barplot", "scatter", "heatmap", "boxplot",
     "violinplot", "histogram", "radar", "area", "stacked_bar",
+    "pareto", "contour", "waterfall", "dumbbell",
     "set_style", "get_style", "set_dpi", "set_font", "set_context",
     "suggest",
     "auto_plot", "AutoPlotResult",
